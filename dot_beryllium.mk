4cd8f065159cf4767305c8bc8a097bf790ed74c3
notakbuzz <74129681+notakbuzz@users.noreply.github.com>
2021-11-21 11:29:58 +0530

    beryllium: prop: change to November fingerprint

--- dot_beryllium.mk
+++ dot_beryllium.mk
@@ -17,10 +17,10 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := POCO F1
 PRODUCT_MANUFACTURER := Xiaomi
 
-BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.211001.001/7641976:user/release-keys"
+BUILD_FINGERPRINT := "google/redfin/redfin:11/RD1A.201105.003/6886239:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.211001.001.A1 7641976 release-keys" \
+    PRIVATE_BUILD_DESC="redfin-user 11 RD1A.201105.003 6886239 release-keys" \
     PRODUCT_NAME="beryllium"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
